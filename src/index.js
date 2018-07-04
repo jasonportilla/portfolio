7b147eb6a109b86c833ad591b50f0335eed73690
jasonportilla <jasonportilla@Jasons-MacBook-Pro.local>
2018-07-04 18:10:50 -0400

    setup webpack and scss

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./style.scss";
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+
+console.log(add);
+console.log(subtract);
+console.log(multiply);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = {
+  entry: { main: './src/index.js'},
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            // fallback to style-loader in development
+            process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {} ),
+    new UglifyJsPlugin({
+      //options here
+      test: /\.js($|\?)/i
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: "[id].css"
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: __dirname + '/src/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
